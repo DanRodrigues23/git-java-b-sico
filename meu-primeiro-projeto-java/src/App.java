bd48f4752dfd40959a518917852c78c4f6960524
Daniel <Peritod.rodrigues@gmail.com>
2024-08-30 17:54:20 -0300

    Meu primeiro projeto java

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World, I'm Daniel Rodrigues, a beginner in the Java language");
+    }
+}
